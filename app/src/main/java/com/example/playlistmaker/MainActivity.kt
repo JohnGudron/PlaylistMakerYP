9218eaee19a7173f5e6505c89779a3336524f8b0
JohnGudron <oak43556@gmail.com>
2025-01-13 20:50:15 +0300

    Completed tasks for 8 sprint.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchBtn = findViewById<Button>(R.id.searchBtn)
+        val mediaBtn = findViewById<Button>(R.id.mediaBtn)
+        val settingsBtn = findViewById<Button>(R.id.settingsBtn)
+
+        val searchBtnOnClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+            }
+        }
+
+        searchBtn.setOnClickListener (searchBtnOnClickListener)
+
+        mediaBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MediaActivity::class.java))
+        }
+
+        settingsBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
         }
     }
 }
